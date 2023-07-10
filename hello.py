344c9ed5ecb68be76162ddbe51b276ca7f35b23f
Prathna Gupta <prathnagupta@Prathnas-MacBook-Air.local>
2023-07-10 04:37:59 -0400

    Add Excel files

--- hello.py
+++ hello.py
@@ -1,1 +1,21 @@
 print("Hello from Binder!")
+import pandas as pd
+
+# Load the dataset
+df = pd.read_excel("./titanic.xls")
+
+# Check for missing values
+print("Missing values:")
+print(df.isnull().sum())
+
+# Remove duplicate rows
+df.drop_duplicates(inplace=True)
+
+# Convert data types
+df['Age'] = df['Age'].astype(float)
+
+# Rename columns
+df.rename(columns={'Pclass': 'PassengerClass', 'Embarked': 'Port'}, inplace=True)
+
+# Save the cleaned dataset
+df.to_excel("titanic_data_cleaned.xlsx", index=False)
